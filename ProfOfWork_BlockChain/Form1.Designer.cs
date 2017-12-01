ba56f8c888babf45f290ba4b967045ccef8ccfe5
Kazakov500 <kazakov.VADIM.2012@Yandex.ru>
2017-12-01 23:55:52 +0300

    first

---
+++ ProfOfWork_BlockChain/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace ProfOfWork_BlockChain
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btn_Start = new System.Windows.Forms.Button();
+            this.rtb_Message = new System.Windows.Forms.RichTextBox();
+            this.rtb_Hash = new System.Windows.Forms.RichTextBox();
+            this.tb_Zeros = new System.Windows.Forms.TextBox();
+            this.rtb_Log = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.nud_Threads_Count = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tb_CountOfIterations = new System.Windows.Forms.TextBox();
+            this.tb_Current_Number = new System.Windows.Forms.TextBox();
+            this.btn_GetCurrHash = new System.Windows.Forms.Button();
+            this.tb_Num = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.nud_Threads_Count)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_Start
+            // 
+            this.btn_Start.Location = new System.Drawing.Point(190, 263);
+            this.btn_Start.Name = "btn_Start";
+            this.btn_Start.Size = new System.Drawing.Size(62, 23);
+            this.btn_Start.TabIndex = 0;
+            this.btn_Start.Text = "Start";
+            this.btn_Start.UseVisualStyleBackColor = true;
+            this.btn_Start.Click += new System.EventHandler(this.btn_Start_Click);
+            // 
+            // rtb_Message
+            // 
+            this.rtb_Message.Location = new System.Drawing.Point(12, 12);
+            this.rtb_Message.Name = "rtb_Message";
+            this.rtb_Message.Size = new System.Drawing.Size(240, 118);
+            this.rtb_Message.TabIndex = 1;
+            this.rtb_Message.Text = "Vadim";
+            // 
+            // rtb_Hash
+            // 
+            this.rtb_Hash.Location = new System.Drawing.Point(12, 136);
+            this.rtb_Hash.Name = "rtb_Hash";
+            this.rtb_Hash.Size = new System.Drawing.Size(240, 96);
+            this.rtb_Hash.TabIndex = 2;
+            this.rtb_Hash.Text = "";
+            // 
+            // tb_Zeros
+            // 
+            this.tb_Zeros.Location = new System.Drawing.Point(64, 238);
+            this.tb_Zeros.Name = "tb_Zeros";
+            this.tb_Zeros.Size = new System.Drawing.Size(188, 20);
+            this.tb_Zeros.TabIndex = 3;
+            this.tb_Zeros.Text = "000000000000000";
+            // 
+            // rtb_Log
+            // 
+            this.rtb_Log.Location = new System.Drawing.Point(258, 12);
+            this.rtb_Log.Name = "rtb_Log";
+            this.rtb_Log.Size = new System.Drawing.Size(203, 246);
+            this.rtb_Log.TabIndex = 4;
+            this.rtb_Log.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 241);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Zeros";
+            // 
+            // nud_Threads_Count
+            // 
+            this.nud_Threads_Count.Location = new System.Drawing.Point(64, 263);
+            this.nud_Threads_Count.Name = "nud_Threads_Count";
+            this.nud_Threads_Count.Size = new System.Drawing.Size(120, 20);
+            this.nud_Threads_Count.TabIndex = 6;
+            this.nud_Threads_Count.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 265);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Threads";
+            // 
+            // tb_CountOfIterations
+            // 
+            this.tb_CountOfIterations.Location = new System.Drawing.Point(64, 318);
+            this.tb_CountOfIterations.Name = "tb_CountOfIterations";
+            this.tb_CountOfIterations.Size = new System.Drawing.Size(100, 20);
+            this.tb_CountOfIterations.TabIndex = 8;
+            this.tb_CountOfIterations.Text = "10000";
+            // 
+            // tb_Current_Number
+            // 
+            this.tb_Current_Number.Location = new System.Drawing.Point(64, 344);
+            this.tb_Current_Number.Name = "tb_Current_Number";
+            this.tb_Current_Number.Size = new System.Drawing.Size(100, 20);
+            this.tb_Current_Number.TabIndex = 9;
+            this.tb_Current_Number.Text = "0";
+            // 
+            // btn_GetCurrHash
+            // 
+            this.btn_GetCurrHash.Location = new System.Drawing.Point(333, 315);
+            this.btn_GetCurrHash.Name = "btn_GetCurrHash";
+            this.btn_GetCurrHash.Size = new System.Drawing.Size(75, 23);
+            this.btn_GetCurrHash.TabIndex = 10;
+            this.btn_GetCurrHash.Text = "Hash";
+            this.btn_GetCurrHash.UseVisualStyleBackColor = true;
+            this.btn_GetCurrHash.Click += new System.EventHandler(this.btn_GetCurrHash_Click);
+            // 
+            // tb_Num
+            // 
+            this.tb_Num.Location = new System.Drawing.Point(227, 317);
+            this.tb_Num.Name = "tb_Num";
+            this.tb_Num.Size = new System.Drawing.Size(100, 20);
+            this.tb_Num.TabIndex = 11;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(473, 384);
+            this.Controls.Add(this.tb_Num);
+            this.Controls.Add(this.btn_GetCurrHash);
+            this.Controls.Add(this.tb_Current_Number);
+            this.Controls.Add(this.tb_CountOfIterations);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.nud_Threads_Count);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rtb_Log);
+            this.Controls.Add(this.tb_Zeros);
+            this.Controls.Add(this.rtb_Hash);
+            this.Controls.Add(this.rtb_Message);
+            this.Controls.Add(this.btn_Start);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.nud_Threads_Count)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Start;
+        private System.Windows.Forms.RichTextBox rtb_Message;
+        private System.Windows.Forms.RichTextBox rtb_Hash;
+        private System.Windows.Forms.TextBox tb_Zeros;
+        private System.Windows.Forms.RichTextBox rtb_Log;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown nud_Threads_Count;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tb_CountOfIterations;
+        private System.Windows.Forms.TextBox tb_Current_Number;
+        private System.Windows.Forms.Button btn_GetCurrHash;
+        private System.Windows.Forms.TextBox tb_Num;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ ProfOfWork_BlockChain/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProfOfWork_BlockChain
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        Task[] tasks;
+        private async void btn_Start_Click(object sender, EventArgs e)
+        {
+
+
+            rtb_Log.Clear();
+            //Get_Number_For_Block();
+
+            //Thread t = new Thread(Get_Number_For_Block);
+            //t.Start(new string[] { rtb_Message.Text, tb_Zeros.Text });
+
+
+            List<int> result;
+
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+
+            try
+            {
+                var progress = new Progress<string>(update =>
+                {
+                    rtb_Log.AppendText(update);
+                    rtb_Log.AppendText(Environment.NewLine);
+                    rtb_Log.Refresh();
+                });
+                var progress_num = new Progress<string>(update =>
+                {
+                    tb_Current_Number.Text = update;
+                });
+                string[] vars = new string[] { rtb_Message.Text, tb_Zeros.Text };
+
+                int count_of_iterations = Convert.ToInt32(tb_CountOfIterations.Text);
+                int count_of_threads = 4;
+                int fold_size = count_of_iterations / count_of_threads;
+
+                //10 thr
+                //1000 iter
+                //100 fold
+                tasks = new Task[count_of_threads];
+                for (int i = 0; i < count_of_threads; i++)
+                {
+                    //await Task.Run(() => Get_Number_For_Block(i * fold_size, (i * fold_size) + fold_size, vars, progress, progress_num));
+                    tasks[i] = Task.Factory.StartNew(() => Get_Number_For_Block(i * fold_size, (i * fold_size) + fold_size, vars, progress, progress_num));
+                    rtb_Log.AppendText(i.ToString() + "thread created\n");
+                }
+                Task.WaitAll(tasks);
+
+            }
+            catch (InvalidOperationException ex) { };
+
+
+
+            watch.Stop();
+            long elapsedMs = watch.ElapsedMilliseconds;
+            rtb_Log.AppendText(elapsedMs.ToString() + " ms\n");
+
+            /*string[] vars = new string[] { rtb_Message.Text, tb_Zeros.Text };
+
+            List<int> result = await Task.Run(() =>
+            {
+                //return Get_Number_For_Block(vars);
+            });*/
+
+            //foreach (var item in result)
+            //    rtb_Log.AppendText(item.ToString() + " " + "\n");
+
+
+        }
+
+        private async void CreateThread(int cot)
+        {
+
+        }
+
+
+        private void Get_Number_For_Block(int start, int end, object str_arr, IProgress<string> progress, IProgress<string>  progress_num)
+        {
+            string Message = ((string[])(str_arr))[0];
+            string First_Zeros = ((string[])(str_arr))[1];
+            string Hash;
+            //List<int> list = new List<int>(); 
+
+            for (int i = start; i < end; i++)
+            {
+
+                string block = Message + "\n" + i.ToString();
+                Hash = hash(block);
+
+                string ss = (Hash.Substring(0, tb_Zeros.Text.Length));
+                bool ss_b = ss.Equals(tb_Zeros.Text);
+
+                if ((Hash.Substring(0, tb_Zeros.Text.Length)).Equals(tb_Zeros.Text))
+                {
+                    //list.Add(i);
+                    progress.Report(i.ToString());
+                    //progress_num.Report(i.ToString());
+                }
+                if ((i % 1000) == 0)
+                {
+                    progress_num.Report(i.ToString());
+                }
+            }
+            //progress.Report("Finish");
+            //return list;
+        }
+
+
+        private void Get_Hash(int Num)
+        {
+            string Message = rtb_Message.Text;
+            string First_Zeros = tb_Zeros.Text;
+            string Hash;
+            
+
+
+
+            string block = Message + "\n" + Num.ToString();
+            Hash = hash(block);
+            rtb_Hash.Text = Hash;
+
+
+
+        }
+
+
+        private string hash(string text)
+        {
+            byte[] data = Encoding.Default.GetBytes(text);
+            byte[] result = new SHA256Managed().ComputeHash(data);
+
+            return string.Join("", result.Select(x => Convert.ToString(x, 2).PadLeft(8, '0')));
+        }
+
+        private void btn_GetCurrHash_Click(object sender, EventArgs e)
+        {
+            rtb_Hash.Clear();
+            Get_Hash(Convert.ToInt32(tb_Num.Text));
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            tb_Current_Number.Refresh();
+            rtb_Hash.Refresh();
+            rtb_Log.Refresh();
+        }
+    }
+}
